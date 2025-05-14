81f0af20005bc3d80af1e0157b0e3068b2e3e4ad
Damian Metheny <metheny555@outlook.com>
2025-05-13 22:06:12 -0400

    Login tracker

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0; 
 
+  const attemptLogin = (passwordAttempt) => {
+    attemptCount++; 
 
+    
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful"; 
+    } else {
+      return `Attempt ${attemptCount}: Login failed`; 
+    }
+  };
+
+  return attemptLogin; 
+}
+
+const userInfo = {
+  username: "user1",
+  password: "password123"
+};
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
